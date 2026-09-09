2e17dd12a2881ba25dcb0e678c576dc5f3b9532c
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2026-09-09 12:15:50 -0300

    Início dos estudos de teste de software

--- calculadora.js
+++ calculadora.js
@@ -15,20 +15,4 @@ function divide(a, b) {
   return a / b;
 }
 
-function ehPar(n) {
-  return n % 2 === 0;
-}
-
-function raiz(n) {
-  if (n < 0) throw new Error('Nao e possivel calcular raiz de numero negativo');
-  return Math.sqrt(n);
-}
-
-function media(numeros) {
-  if (!Array.isArray(numeros) || numeros.length === 0) {
-    throw new Error('A lista de numeros nao pode ser vazia');
-  }
-  return numeros.reduce((total, n) => total + n, 0) / numeros.length;
-}
-
-module.exports = { soma, subtrai, multiplica, divide, ehPar, raiz, media };
+module.exports = { soma, subtrai, multiplica, divide };
